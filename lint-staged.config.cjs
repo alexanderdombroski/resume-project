cb80e04c32ba66bce3027000d208c3567990dbc8
Alex Dombroski <alexdombroski1@gmail.com>
2026-05-05 20:22:49 -0600

    feat(cicd): setup deploy workflow

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -3,7 +3,7 @@ const joinFiles = (files) => files.map((file) => `"${file}"`).join(' ');
 module.exports = {
   '*.{ts,js,mjs,cjs,mts,astro,svelte}': (stagedFiles) => {
     const files = joinFiles(stagedFiles);
-    return [`prettier --write ${files}`, `eslint --max-warnings=0 ${files}`];
+    return [`prettier --write ${files}`, `eslint --max-warnings=0 --no-warn-ignored ${files}`];
   },
   '*.{md,css}': (stagedFiles) => [`prettier --write ${joinFiles(stagedFiles)}`],
 };
