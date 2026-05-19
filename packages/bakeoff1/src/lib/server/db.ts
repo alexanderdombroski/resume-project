730786b71116be02c2a3d301eac7a8eb2abbabdd
Alex Dombroski <alexdombroski1@gmail.com>
2026-05-19 06:06:23 -0600

    Revert "fix: limit db connection times"
    This reverts commit 749bd6056e8de132645f312d453c4b5a906defae.

--- packages/bakeoff1/src/lib/server/db.ts
+++ packages/bakeoff1/src/lib/server/db.ts
@@ -6,9 +6,4 @@ const { Pool } = pg;
 export const db = new Pool({
   connectionString: DATABASE_URI,
   ssl: import.meta.env.PROD,
-  // Prevent indefinitely pending DB calls in Cloudflare Workers.
-  connectionTimeoutMillis: 10_000,
-  idleTimeoutMillis: 30_000,
-  query_timeout: 15_000,
-  statement_timeout: 15_000,
 });
