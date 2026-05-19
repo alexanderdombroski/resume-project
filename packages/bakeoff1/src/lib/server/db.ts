9ed482a0a6a58340890c5bc29d45313598684cbc
Alex Dombroski <alexdombroski1@gmail.com>
2026-05-19 06:17:32 -0600

    fix: close when done

--- packages/bakeoff1/src/lib/server/db.ts
+++ packages/bakeoff1/src/lib/server/db.ts
@@ -4,16 +4,16 @@ import { DATABASE_URI } from '$env/static/private';
 const { Pool } = pg;
 type PoolClient = pg.PoolClient;
 
-export const db = new Pool({
-  connectionString: DATABASE_URI,
-  ssl: import.meta.env.PROD,
-});
-
 export async function withDbClient<T>(fn: (client: PoolClient) => Promise<T>): Promise<T> {
+  const db = new Pool({
+    connectionString: DATABASE_URI,
+    ssl: import.meta.env.PROD,
+  });
   const client = await db.connect();
   try {
     return await fn(client);
   } finally {
     client.release();
+    db.end();
   }
 }
