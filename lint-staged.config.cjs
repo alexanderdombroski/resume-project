18d7973f5c98edefd3a68cbca1e809e7d5c3cac8
Alex Dombroski <alexdombroski1@gmail.com>
2026-06-03 12:05:02 -0600

    fix: tsx files correctly linted during precommit

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,7 @@
 const joinFiles = (files) => files.map((file) => `"${file}"`).join(' ');
 
 module.exports = {
-  '*.{ts,js,mjs,cjs,mts,astro,svelte,vue}': (stagedFiles) => {
+  '*.{ts,js,mjs,cjs,mts,astro,svelte,vue,tsx}': (stagedFiles) => {
     const files = joinFiles(stagedFiles);
     return [`prettier --write ${files}`, `eslint --max-warnings=0 --no-warn-ignored ${files}`];
   },
