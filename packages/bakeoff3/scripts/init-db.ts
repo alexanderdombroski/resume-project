7b1f35faca6b05ecf9fb6c8acd18ffba96076502
Alex Dombroski <alexdombroski1@gmail.com>
2026-06-04 14:06:13 -0600

    fix: db init

--- packages/bakeoff3/scripts/init-db.ts
+++ packages/bakeoff3/scripts/init-db.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+// @ts-ignore
 import { createDbClient, runSqlBatch } from '../src/utils/db.ts';
 
-// node --experimental-strip-types scripts/init-db.ts
+// node --env-file=".env" scripts/init-db.ts
 // Set TURSO_DATABASE_URL and TURSO_AUTH_TOKEN for a Turso database.
 
 const sqlPath = path.join(import.meta.dirname, 'init-db.sql');
